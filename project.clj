2d3964705eb2a782f195ad0108c028ddfeed2fab
du.kulaevskiy <du.kulaevskiy@tensor.ru>
2016-12-12 15:58:11 +0300

    logs

--- project.clj
+++ project.clj
@@ -7,8 +7,13 @@
                  [org.clojars.scsibug/feedparser-clj "0.4.0"]
                  [tblibrary "0.1.2-SNAPSHOT"]
                  [org.clojure/core.async "0.2.374"]
+                 [org.clojure/data.json "0.2.6"]
+                 ;; Logs
                  [org.clojure/tools.logging "0.3.1"]
-                 [org.clojure/data.json "0.2.6"]]
+                 [log4j/log4j "1.2.17" :exclusions [javax.mail/mail
+                                                    javax.jms/jms
+                                                    com.sun.jmdk/jmxtools
+                                                    com.sun.jmx/jmxri]]]
   :main ^:skip-aot rss-get.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
