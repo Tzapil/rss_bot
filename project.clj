2f84c4d2a89b311acd1dffcff6727bc02ab92e5f
du.kulaevskiy <du.kulaevskiy@tensor.ru>
2016-12-12 15:21:46 +0300

    change lib version

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojars.scsibug/feedparser-clj "0.4.0"]
-                 [tblibrary "0.1.0-SNAPSHOT"]
+                 [tblibrary "0.1.2-SNAPSHOT"]
                  [org.clojure/core.async "0.2.374"]
                  [org.clojure/tools.logging "0.3.1"]
                  [org.clojure/data.json "0.2.6"]]
